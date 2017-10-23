a63daf9bbb674b1b1a9ca714bac1c363ffae2fe8
hejinlong <hejinlong@haixun360.com>
2017-10-23 18:16:42 +0800

    '修改了主题'

--- app/src/main/java/com/hejin/news/MainApp.java
+++ app/src/main/java/com/hejin/news/MainApp.java
@@ -2,6 +2,8 @@ package com.hejin.news;
 
 import android.app.Application;
 
+import com.alibaba.android.arouter.launcher.ARouter;
+
 /**
  * 作者: *贺金龙
  * 创建时间: *  2017/10/20 17:19
@@ -13,4 +15,22 @@ import android.app.Application;
  *  
  */
 public class MainApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initARouter();
+    }
+
+    /**
+     * <p>
+     * 说明:初始化ARouter
+     * </p>
+     */
+    private void initARouter() {
+        if (true) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog();     // 打印日志
+            ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(this); // 尽可能早，推荐在Application中初始化
+    }
 }
